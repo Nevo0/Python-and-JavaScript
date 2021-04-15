b59b3c433456aff22af1a65e1f4a03d66602f98c
Rafal <piecyk.rafal@gmial.com>
2021-04-16 00:20:11 +0200

    Functions

---
+++ importpyfile.py
@@ -0,0 +1,9 @@
+from functioninport import square # mozemy inportowac wszystko *
+
+for i in range(9):
+    print(f"The square of {i} is {square(i)}" )
+
+import functioninport
+
+for i in range(9):
+    print(f"The square of {i} is {functioninport.square(i)}" )
