d107cc8b53d602c1a7bb2119db78ce3fc8e53fa3
Rafal <piecyk.rafal@gmial.com>
2021-04-15 23:04:42 +0200

    Add file list and set

---
+++ list.py
@@ -0,0 +1,11 @@
+# Define a list name
+names = ["Hary", "Rony", "Hermione", "Aura"]
+
+print(names)
+print(names[0])
+print(names[2])
+
+names.append("Draco")
+names.sort()
+
+print(names) 
---
+++ set.py
@@ -0,0 +1,17 @@
+
+s= set()
+
+#Add element to set
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(4)
+s.add(5)
+s.add(5)
+
+s.remove(2)
+
+print(s)
+print(f"The set has {len(s)} elements")
